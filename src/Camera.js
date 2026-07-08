328b1b0c7011c4f140db12b4c2c40d2b9861f961
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-08 16:58:06 -0400

    first commit

---
+++ src/Camera.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three'
+
+export function createCamera() {
+  const fov = 40;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 50, 0);
+  camera.up.set(0, 0, 1);
+  camera.lookAt(0, 0, 0);
+
+  return camera
+}
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import { createCamera } from './Camera'
+import './style.css'
+import * as THREE from 'three'
+
+
+const canvas = document.querySelector('#c')
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas})
+
+const scene = new THREE.Scene()
+const camera = createCamera()
+scene.add(camera)
+
+
+function resizeRendererToDisplaySize(renderer, maxPixelCount=3840*2160) {
+
+  const pixelRatio = window.devicePixelRatio;
+  let width  = Math.floor( canvas.clientWidth  * pixelRatio );
+  let height = Math.floor( canvas.clientHeight * pixelRatio );
+  const pixelCount = width * height;
+  const renderScale = pixelCount > maxPixelCount ? Math.sqrt(maxPixelCount / pixelCount) : 1;
+  width = Math.floor(width * renderScale);
+  height = Math.floor(height * renderScale);
+
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false)
+  }
+
+  return needResize
+}
+
+
+function animate( time ) {
+  time *= 0.001; //convert time to seconds
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight
+    camera.updateProjectionMatrix()
+  }
+
+
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop( animate );
+
