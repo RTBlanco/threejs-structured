0496a5deafb4b60699a96415e7b52bed7cf9f7bb
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-08 20:16:33 -0400

    added small controll to sphere

--- src/Sphere.js
+++ src/Sphere.js
@@ -12,4 +12,52 @@ export class SphereObject {
 
     scene.add(this.sphereMesh)
   }
+
+  _moveUp() {
+    console.log('up')
+    this.sphereMesh.position.x -= .001
+  }
+
+  _moveDown() {
+    console.log('down')
+    this.sphereMesh.position.x += .001
+  }
+
+  _moveLeft(){
+    console.log('left')
+    this.sphereMesh.position.z += .001
+  }
+  
+  _moveRight(){
+    console.log('right')
+    this.sphereMesh.position.z -= .001
+  }
+
+  _move() {
+    addEventListener('keydown', (e) => {
+
+      switch (e.key) {
+        case 'ArrowUp':
+          this._moveUp() 
+          break;
+        case 'ArrowDown':
+          this._moveDown() 
+          break;
+
+        case 'ArrowLeft':
+          this._moveLeft()
+          break;
+
+        case 'ArrowRight':
+          this._moveRight() 
+          break;
+
+        default:
+          break;
+      }
+    })
+  }
+  update(time){
+    this._move()
+  }
 }
