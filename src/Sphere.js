f48cc01aae34476af3fc322e31b49bc77c7467f3
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-09 07:01:51 -0400

    added damping

--- src/Sphere.js
+++ src/Sphere.js
@@ -1,65 +1,72 @@
 import * as THREE from 'three'
 
-  export class SphereObject {
-    constructor(scene) {
-      const geometry = new THREE.SphereGeometry(1, 24, 30)
-      const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
-      
-      this.sphereMesh = new THREE.Mesh(geometry, material)
+export class SphereObject {
+  constructor(scene) {
+    const geometry = new THREE.SphereGeometry(1, 24, 30)
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+    
+    this.sphereMesh = new THREE.Mesh(geometry, material)
 
-      const edgesGeometry = new THREE.EdgesGeometry(geometry, 1)
-      const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 })
-      const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial)
-      this.sphereMesh.add(edges)
+    const edgesGeometry = new THREE.EdgesGeometry(geometry, 1)
+    const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 })
+    const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial)
+    this.sphereMesh.add(edges)
 
-      this.sphereMesh.position.y = 1
-      scene.add(this.sphereMesh)
+    this.sphereMesh.position.y = 1
+    scene.add(this.sphereMesh)
 
-      this.speed = 5
-      this.keys = {}
+    this.speed = 1
+    this.keys = {}
 
-      window.addEventListener('keydown', e => {
-        this.keys[e.code] = true
-      })
+    this.velocity = new THREE.Vector3(0, 0, 0)
+    this.friction = .97
 
-      window.addEventListener('keyup', e => {
-        this.keys[e.code] = false
-      })
+    window.addEventListener('keydown', e => {
+      this.keys[e.code] = true
+    })
 
+    window.addEventListener('keyup', e => {
+      this.keys[e.code] = false
+    })
+  }
+
+  update(deltaTime) {
+    const acceleration = this.speed * deltaTime
+
+    if (this.keys["ArrowUp"]) {
+      this.velocity.x -= acceleration
     }
 
+    if (this.keys["ArrowDown"]) {
+      this.velocity.x += acceleration
+    }
 
+    if (this.keys["ArrowLeft"]) {
+      this.velocity.z += acceleration
+    }
 
-    update(deltaTime) {
-      const distance = this.speed * deltaTime
+    if (this.keys["ArrowRight"]) {
+      this.velocity.z -= acceleration
+    }
 
+    // apply movement from velocity
+    this.sphereMesh.position.x += this.velocity.x
+    this.sphereMesh.position.z += this.velocity.z
 
-      if (this.keys["ArrowUp"]) {
-        this.sphereMesh.position.x -= distance
-        this.sphereMesh.rotation.z += distance
-      }
+    // rotate based on velocity
+    this.sphereMesh.rotation.z -= this.velocity.x
+    this.sphereMesh.rotation.x += this.velocity.z
 
-      if (this.keys['ArrowDown']) {
-        this.sphereMesh.position.x += distance
-        this.sphereMesh.rotation.z -= distance
-      }
+    // friction / damping
+    this.velocity.x *= this.friction
+    this.velocity.z *= this.friction
 
-      if (this.keys['ArrowLeft']) {
-        this.sphereMesh.position.z += distance
-        this.sphereMesh.rotation.x += distance
-      }
+    if (this.keys["Space"]) {
+      this.sphereMesh.position.y += acceleration + 1
+    }
 
-      if (this.keys['ArrowRight']) {
-        this.sphereMesh.position.z -= distance
-        this.sphereMesh.rotation.x -= distance
-      }
-
-      if(this.keys['Space']) {
-        this.sphereMesh.position.y += distance + 1
-      }
-
-      if (this.sphereMesh.position.y > 1){
-        this.sphereMesh.position.y -= distance + .2 
-      }
+    if (this.sphereMesh.position.y > 1) {
+      this.sphereMesh.position.y -= acceleration + 0.5
     }
   }
+}
