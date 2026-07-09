d525be53492fdff8fd782a05edda7ca61db77661
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-09 19:56:11 -0400

    changed movment to work with physics engine

--- src/Sphere.js
+++ src/Sphere.js
@@ -8,29 +8,45 @@ export class SphereObject {
     const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
     
     this.sphereMesh = new THREE.Mesh(geometry, material)
-
-    const edgesGeometry = new THREE.EdgesGeometry(geometry, 1)
-    const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 })
-    const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial)
-
-    this.sphereMesh.add(edges)
-
     this.sphereMesh.position.y = 10
 
-    this.speed = 1
-    this.keys = {}
+    // this.speed = 1
+    // this.keys = {}
 
-    this.velocity = new THREE.Vector3(0, 0, 0)
-    this.friction = .97
 
-    window.addEventListener('keydown', e => {
-      this.keys[e.code] = true
-    })
+    // window.addEventListener('keydown', e => {
+    //   this.keys[e.code] = true
+    // })
 
-    window.addEventListener('keyup', e => {
-      this.keys[e.code] = false
-    })
+    // window.addEventListener('keyup', e => {
+    //   this.keys[e.code] = false
+    // })
 
+    // Movement input
+    this.movement = { forward: 0, right: 0 };
+
+    window.addEventListener( 'keydown', ( event ) => {
+
+      if ( event.key === 'w' || event.key === 'ArrowUp' ) this.movement.forward = 1;
+      if ( event.key === 's' || event.key === 'ArrowDown' ) this.movement.forward = - 1;
+      if ( event.key === 'a' || event.key === 'ArrowLeft' ) this.movement.right = - 1;
+      if ( event.key === 'd' || event.key === 'ArrowRight' ) this.movement.right = 1;
+  
+    } );
+
+    window.addEventListener( 'keyup', ( event ) => {
+  
+      if ( event.key === 'w' || event.key === 's' || event.key === 'ArrowUp' || event.key === 'ArrowDown' ) this.movement.forward = 0;
+      if ( event.key === 'a' || event.key === 'd' || event.key === 'ArrowLeft' || event.key === 'ArrowRight' ) this.movement.right = 0;
+  
+    } );
+
+
+    // this.characterController = physics.world.createCharacterController( 0.01 );
+    // this.characterController.setApplyImpulsesToDynamicBodies( true );
+    // this.characterController.setCharacterMass( 3 );
+    // const colliderDesc = physics.RAPIER.ColliderDesc.capsule( 0.5, 0.3 ).setTranslation( 0, 0.8, 0 );
+    // this.sphereMesh.userData.collider = physics.world.createCollider( colliderDesc );
 
   }
 
@@ -38,36 +54,59 @@ export class SphereObject {
     return this.sphereMesh
   }
 
+  // update(deltaTime) {
+  //   const acceleration = this.speed * deltaTime
+
+  //   if (this.keys["ArrowUp"]) {
+  //     this.velocity.x -= acceleration
+  //   }
+
+  //   if (this.keys["ArrowDown"]) {
+  //     this.velocity.x += acceleration
+  //   }
+
+  //   if (this.keys["ArrowLeft"]) {
+  //     this.velocity.z += acceleration
+  //   }
+
+  //   if (this.keys["ArrowRight"]) {
+  //     this.velocity.z -= acceleration
+  //   }
+
+  //   // apply movement from velocity
+  //   this.sphereMesh.position.x += this.velocity.x
+  //   this.sphereMesh.position.z += this.velocity.z
+
+  //   // friction / damping
+  //   this.velocity.x *= this.friction
+  //   this.velocity.z *= this.friction
+
+  //   if (this.keys["Space"]) {
+  //     this.sphereMesh.position.y += acceleration + 1
+  //   }
+
+  // }
+
   update(deltaTime) {
-    const acceleration = this.speed * deltaTime
+    // const deltaTime = 1 / 60;
 
-    if (this.keys["ArrowUp"]) {
-      this.velocity.x -= acceleration
-    }
+    // Character movement
+    // const speed = 2.5 * deltaTime;
+    // const moveVector = new physics.RAPIER.Vector3( movement.right * speed, 0, - movement.forward * speed );
 
-    if (this.keys["ArrowDown"]) {
-      this.velocity.x += acceleration
-    }
+    // characterController.computeColliderMovement( player.userData.collider, moveVector );
 
-    if (this.keys["ArrowLeft"]) {
-      this.velocity.z += acceleration
-    }
+    // // Read the result.
+    // const translation = characterController.computedMovement();
+    // const position = player.userData.collider.translation();
 
-    if (this.keys["ArrowRight"]) {
-      this.velocity.z -= acceleration
-    }
+    // position.x += translation.x;
+    // position.y += translation.y;
+    // position.z += translation.z;
 
-    // apply movement from velocity
-    this.sphereMesh.position.x += this.velocity.x
-    this.sphereMesh.position.z += this.velocity.z
+    // player.userData.collider.setTranslation( position );
 
-    // friction / damping
-    this.velocity.x *= this.friction
-    this.velocity.z *= this.friction
-
-    if (this.keys["Space"]) {
-      this.sphereMesh.position.y += acceleration + 1
-    }
-
+    // // Sync Three.js mesh with Rapier collider
+    // player.position.set( position.x, position.y, position.z );
   }
 }
