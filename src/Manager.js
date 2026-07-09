e1769f0b2d8285da9310d831dd78392223517bba
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-09 16:56:52 -0400

    controlled scene here

--- src/Manager.js
+++ src/Manager.js
@@ -5,6 +5,9 @@ import { PlaneObject } from './Plane';
 import { LightObject } from './Light';
 import { SphereObject } from './Sphere';
 
+import { RapierPhysics } from 'three/addons/physics/RapierPhysics.js';
+import { RapierHelper } from 'three/addons/helpers/RapierHelper.js';
+
 export class Manager {
   constructor(canvas, scenes=[], cameras=[]) {
     this.canvas = canvas
@@ -13,16 +16,18 @@ export class Manager {
     this.activeScene = scenes.length > 0 ? scenes[0] : new THREE.Scene()
     this.cameras = cameras
     this.activeCamera = cameras.length > 0 ? cameras[0]: new CameraObject()
+    this.initPhysics();
     this.objects = [
-      new PlaneObject(this.activeScene),
-      new LightObject(this.activeScene), 
-      new SphereObject(this.activeScene)
+      new PlaneObject(),
+      new LightObject(), 
+      new SphereObject()
     ]
 
     const controls = new OrbitControls(this.activeCamera, this.canvas)
     controls.target.set(0,5,0)
     controls.update()
 
+    
   }
 
 
@@ -41,6 +46,31 @@ export class Manager {
 
 
     this.renderer.render(this.activeScene, this.activeCamera);
+  }
+
+
+
+  async initPhysics() {
+
+    //Initialize physics engine using the script in the jsm/physics folder
+    this.physics = await RapierPhysics();
+
+    this.physics.addScene( this.activeScene );
+
+    // addBody( );
+
+    //Optionally display collider outlines
+    const physicsHelper = new RapierHelper( this.physics.world );
+    this.activeScene  .add( physicsHelper );
+
+    // setInterval( addBody, 1000 );
+
+  }
+
+  _addToScene(items){
+    for(let i = 0; i < items.length; i ++){
+      this.activeScene.add(items[i])
+    }
   }
 
   _resizeRendererToDisplaySize(renderer, maxPixelCount=3840*2160) {
